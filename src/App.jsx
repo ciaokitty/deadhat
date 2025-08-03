f1c2d38ee51ef4fca54e26fbf31da860ff2ff22a
ciaokitty <sharmasanvi125@gmail.com>
2025-08-03 23:11:33 +0530

    nice orb

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import { Particles } from './Particles'
 
 export default function App() {
   const props = useControls({
-    focus: { value: 5.1, min: 3, max: 7, step: 0.01 },
-    speed: { value: 100, min: 0.1, max: 100, step: 0.1 },
-    aperture: { value: 1.8, min: 1, max: 5.6, step: 0.1 },
-    fov: { value: 20, min: 0, max: 200 },
-    curl: { value: 0.25, min: 0.01, max: 0.5, step: 0.01 }
+    focus: { value: 5.1, min: 4.5, max: 7.5, step: 0.01 },
+    speed: { value: 75, min: 0.1, max: 100, step: 0.1 },
+    aperture: { value: 5.5, min: 1, max: 5.6, step: 0.1 },
+    fov: { value: 90, min: 0, max: 200 },
+    curl: { value: 0.30, min: 0.01, max: 0.5, step: 0.01 }
   })
   return (
     <>
